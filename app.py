8883cadb08fe5e9b903c2d03440101889b6dd9a8
Ramesh Sunkara <ramesh@mac.local>
2020-09-22 15:00:30 -0400

    Initial commit , added REST API to access items, stores and user auth

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from datetime import timedelta
+from db import db
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'ramesh'
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+# /login - change the authentical url to login instead of auth
+# app.config['JWT_AUTH_URL_RULE'] = '/login'
+jwt = JWT(app, authenticate, identity)  # /auth
+
+# Config JWT to expire within half an hour
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+
+# Config JWT auth key name to be "email" instead of default "username"
+# app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
